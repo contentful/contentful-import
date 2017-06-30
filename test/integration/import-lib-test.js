2ef0f66131a9f9a0a35091902a722898342d9041
Benedikt Rötsch <benedikt@contentful.com>
2017-06-30 11:29:57 +0200

    feat(display): implements listr and proper error display

--- test/integration/import-lib-test.js
+++ test/integration/import-lib-test.js
@@ -1,4 +1,4 @@
-import test from 'tape'
+import test from 'blue-tape'
 import runContentfulImport from '../../dist/index'
 import { createClient } from 'contentful-management'
 import { join } from 'path'
@@ -10,10 +10,19 @@ test('It should import a space properly when used as a lib', (t) => {
   const client = createClient({ accessToken: managementToken })
   return client.createSpace({name: 'temp contentful-import space'}, orgId)
     .then((space) => {
-      runContentfulImport({ spaceId: space.sys.id, managementToken: managementToken, contentFile: sampleSpaceFile })
+      runContentfulImport({
+        spaceId: space.sys.id,
+        managementToken,
+        contentFile: sampleSpaceFile,
+        useVerboseRenderer: true
+      })
+        .catch((multierror) => {
+          const failedPublishErrors = multierror.errors.filter((error) => error.hasOwnProperty('error') && error.error.message.indexOf('Could not publish the following entities') !== -1)
+          t.equals(failedPublishErrors.length, 0, 'Is able to publish all entities and does NOT display the failed queue message')
+        })
         .then(() => {
+          t.pass('Finished import as a lib')
           return space.delete()
-            .then(t.end)
         })
     })
 })
