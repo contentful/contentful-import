f0cd91b37ad73f6795ee5ca100ebf352cff95164
Marco Link <marco.link@contentful.com>
2021-01-05 12:00:11 +0100

    fix(webhooks): url validation
    This fix allows the usage of JSON placeholders in webhook urls.
    
    fixes #369 and https://github.com/contentful/contentful-cli/issues/577

---
+++ test/unit/utils/schema.test.js
@@ -0,0 +1,25 @@
+import { webhookSchema } from '../../../lib/utils/schema'
+import Joi from 'joi'
+
+describe('webhooks schema', () => {
+  test('with valid url', () => {
+    expect(Joi.assert('https://www.contentful.com', webhookSchema.url)).toBeUndefined()
+  })
+  test('with invalid url', () => {
+    try {
+      Joi.assert('ttps://www.contentful.com', webhookSchema.url)
+    } catch (e) {
+      expect(e.name).toBe('ValidationError')
+    }
+  })
+  test('with valid url containing custom payload definition', () => {
+    expect(Joi.assert('https://www.contentful.com/webhooks/{ /payload/sys/id }', webhookSchema.url)).toBeUndefined()
+  })
+  test('with invalid url containing custom payload definition', () => {
+    try {
+      Joi.assert('https://www.contentful.com/webhooks/{ /payload/sys/id ', webhookSchema.url)
+    } catch (e) {
+      expect(e.name).toBe('ValidationError')
+    }
+  })
+})
