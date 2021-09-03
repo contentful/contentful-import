19fef6a062399e75e77a990ba7ec37e18a909af9
Janko Marklein <43542437+ruderngespra@users.noreply.github.com>
2021-09-03 10:01:16 +0200

    fix(webhooks): support multiple placeholders [ZEND-1275] (#518)
    * fix(webhooks): add tests for multiple placeholders
    
    fix(webhooks): adjust test description
    
    * fix(webhooks): replace all placeholders

--- test/unit/utils/schema.test.js
+++ test/unit/utils/schema.test.js
@@ -22,4 +22,16 @@ describe('webhooks schema', () => {
       expect(e.name).toBe('ValidationError')
     }
   })
+  test('with valid url containing more than one custom payload definition', () => {
+    expect(
+      Joi.assert('https://www.contentful.com/webhooks/{ /payload/sys/id }/{ /payload/sys/id }', webhookSchema.url)
+    ).toBeUndefined()
+  })
+  test('with invalid url containing more than one custom payload definition', () => {
+    try {
+      Joi.assert('https://www.contentful.com/webhooks/{ /payload/sys/id }/{ /payload/sys/id ', webhookSchema.url)
+    } catch (e) {
+      expect(e.name).toBe('ValidationError')
+    }
+  })
 })
