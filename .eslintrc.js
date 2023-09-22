6679d08c099a2829d1a2a1a299337250e1a1bd61
Janko Marklein <janko.marklein@contentful.com>
2023-09-22 17:46:19 +0200

    chore: use typescript compatible eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,14 @@
 module.exports = {
-  parser: '@babel/eslint-parser',
-  extends: ['standard', 'plugin:jest/recommended'],
-  plugins: ['standard', 'promise'],
-}
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "standard",
+    "plugin:jest/recommended",
+    "plugin:@typescript-eslint/recommended", // Recommended TypeScript rules
+  ],
+  plugins: ["standard", "promise", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-var-requires": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "space-before-function-paren": "warn",
+  },
+};
